0e4f163b7b7ebdeb172c865fadb49aeb7be0ba9f
Mark Joshua Dueñas <duenasmarkjoshua95@gmail.com>
2024-05-22 12:56:33 +0800

    optimize the design of loginpage

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="container mt-5 col-3">
+  <div class="container mt-5 col-6">
     <div class="row">
-      <h4>Enter your email and password</h4>
+      <h4 class="text-blue">Enter your email and password</h4>
       <form @submit.prevent="loginUser" class="col-12">
         <div class="form-group mt-3">
-          <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email" required @input="clearErrors">
+          <input type="email" class="form-control bg-light-blue text-dark" id="email" v-model="email"
+            placeholder="Enter email" required @input="clearErrors">
           <small class="text-danger" v-if="errors">{{ errors }}</small>
         </div>
         <div class="form-group mt-3">
-          <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required @input="clearErrors">
-
+          <input type="password" class="form-control bg-light-blue text-dark" id="password" v-model="password"
+            placeholder="Password" required @input="clearErrors">
         </div>
-        <button type="submit" class="btn btn-primary mt-3 w-100">Login</button>
+        <button type="submit" class="btn btn-primary mt-3 w-100 bg-blue">Login</button>
       </form>
     </div>
     <div class="row">
       <div class="col-12">
-          <p class="mt-3 text-left">Don't have an account? <router-link to="/register">Register here!</router-link></p>
+        <p class="mt-3 text-left text-blue">Don't have an account? <router-link to="/register"
+            class="text-blue">Register here!</router-link></p>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 
@@ -63,3 +66,30 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.text-blue {
+  color: #007bff !important;
+}
+
+.bg-blue {
+  background-color: #007bff !important;
+}
+
+.bg-light-blue {
+  background-color: #cce5ff !important;
+}
+
+.text-dark {
+  color: #343a40 !important;
+}
+
+.btn-primary {
+  border: none;
+  transition: all 0.3s ease;
+}
+
+.btn-primary:hover {
+  background-color: #0056b3 !important;
+}
+</style>
